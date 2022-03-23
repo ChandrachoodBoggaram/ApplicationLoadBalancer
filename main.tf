928db7984d034e7c9f4a0a8a5f7122b8b0056f27
Chandrachood Boggaram <chandrachood.boggaram@syncron.com>
2022-03-23 16:08:08 +0530

    Main file has be commited

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform{
+    required_version = "~>1.1.0"
+    required_providers {
+      aws={
+          source="hashicorp/aws"
+          version="~>3.0"
+      }
+    }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "SG-1" {
+  vpc_id = "vpc-0ce85cf4afc3b65fe"
+
+  ingress  {
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+
+  ingress  {
+    from_port=80
+    to_port=80
+    protocol="tcp"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+
+  egress  {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "Test-sg"
+    "owner"= "Chabog"
+  }
+}
+
+
+resource "aws_instance" "EC2-1" {
+  ami="ami-0069d66985b09d219"
+  instance_type = "t2.micro"
+  key_name = "chandu1"
+  subnet_id = "subnet-0fba361078f7feac1"
+  vpc_security_group_ids = [aws_security_group.SG-1.id]
+  user_data = <<-EOF
+    #! /bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo service httpd start
+    sudo systemctl enable httpd
+    echo "<h1>Hi,This is Chandu Boggaram </h1>" > /var/www/html/index.html
+  EOF
+  tags = {
+      "Name"="Ec2-1"
+  }
+}
